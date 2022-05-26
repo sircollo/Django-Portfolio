3f2095f65e81d82caea1bc797cee9257fbce946e
Collins Sirwani <sircollo71@gmail.com>
2022-05-26 15:10:07 +0300

    Update admin panel view

--- main/admin.py
+++ main/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+  filter_horizontal = ('tags',)
+admin.site.register(Project,ProjectAdmin)
 admin.site.register(tags)
