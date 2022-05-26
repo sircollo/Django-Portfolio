877c597f3f167a9682640ba11df8138a5832710b
Collins Sirwani <sircollo71@gmail.com>
2022-05-26 13:28:25 +0300

    define views for about and projects page

--- main/views.py
+++ main/views.py
@@ -7,3 +7,11 @@ from django.http  import HttpResponse
 def home(request):
     return render(request, 'index.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+
