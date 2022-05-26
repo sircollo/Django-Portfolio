a46a7c130feb4e3ec70a3fe14cb86cbf2ef9c329
Collins Sirwani <sircollo71@gmail.com>
2022-05-26 19:43:47 +0300

    This will add static route for uploaded files location

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import re_path as url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns =[
@@ -7,3 +9,5 @@ urlpatterns =[
   url('^about',views.about,name='about'),
   url('^projects',views.projects,name='projects')
 ]
+if settings.DEBUG:
+  urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
