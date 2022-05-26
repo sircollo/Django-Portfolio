aa34d415d1e1ff9f2cb814c7bef1ecee55066a2c
Collins Sirwani <sircollo71@gmail.com>
2022-05-26 14:57:11 +0300

    This will create project model class and tag model class

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class tags(models.Model):
+  name = models.CharField(max_length=30)
+  
+  def __str__(self):
+    return self.name
+  
+class Project(models.Model):
+  project_name = models.CharField(max_length=30)
+  project_title = models.CharField(max_length=30)
+  project_description = models.TextField()
+  creation_date = models.DateTimeField(auto_now_add=True)
+  tags = models.ManyToManyField(tags)
+  
+  def __str__(self):
+    return self.project_name
+  
+
